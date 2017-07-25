0b8ffedab75be67eebc5daf6eec4b7f21157f0d7
ExpoCode Tech <expocodetech@gmail.com>
2017-07-25 08:11:45 +0200

    Implementación del Servicio DownloadManager para descaga de fichero en Android

---
+++ app/src/main/java/com/expocodetech/android/ectdownloadmanager/MainActivity.java
@@ -0,0 +1,105 @@
+package com.expocodetech.android.ectdownloadmanager;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.IllegalFormatCodePointException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private static final String FILE_URL = "https://expocodetech.com/apps/downloads/fichero.txt";
+    private static final String FILE_NAME = "fichero.txt";
+
+    private DownloadManagerReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (isDownloadManagerAvailable(this)) {
+            registerUnregisterBroadcastReceiver(false);
+            downloadBigFile(FILE_URL, FILE_NAME);
+        }
+    }
+
+    private void registerUnregisterBroadcastReceiver(boolean unregister) {
+        if (!isDownloadManagerAvailable(this))
+            return;
+        if (unregister) {
+            //Desregistramos el DownloadManagerReceiver
+            if (mReceiver != null)
+                unregisterReceiver(mReceiver);
+        } else {
+            //Creamos un IntentFilter para que nuestro DownloadManagerReceiver solo cuando
+            // el usuario haya hecho click en la notificación que el DownloadManage ha desplegado
+            // por nosotros
+            IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_NOTIFICATION_CLICKED);
+            mReceiver = new DownloadManagerReceiver();
+            //Registramos el DownloadManagerReceiver
+            registerReceiver(mReceiver, intentFilter);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        registerUnregisterBroadcastReceiver(true);
+        super.onStop();
+    }
+
+    public static boolean isDownloadManagerAvailable(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            return true;
+        }
+        return false;
+    }
+
+    public void downloadBigFile(String url, String fileName){
+        //Instanciamos una petición de descarga de fichero
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        //Seteamos una descripción para la notificación
+        request.setDescription("El fichero se ha descargado completamente");
+        //Seteamos un título para la notificación
+        request.setTitle("ECTDownloadManager");
+        //Permitimos que el fichero a descargar sea escaneado por el MediaScanner
+        request.allowScanningByMediaScanner();
+        //Indicamos en la petición cuando queremos ahcer visible la Notificación
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        //Le decimos
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+        // Obtenemos el servicio DownloadManager y enviamos a la cola de descargar el fichero que queremosdescargar
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+    }
+
+    public class DownloadManagerReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Este metodo se disparara cuando el usuario haga click sobre la notifiación que el
+            // DownloadManager despliega por nosotros despues de haber culminado la descarga del fichero
+            Log.d(TAG, "DownloadManagerReceiver onReceive");
+            if (intent.getAction().equalsIgnoreCase(DownloadManager.ACTION_NOTIFICATION_CLICKED)){
+
+            }
+        }
+    }
+
+
+}
+
+
+
